c588348b8f17add04f3ac6b344dba9b8307b384d
박예지 <1995.10.21@hanmail.net>
2024-02-23 09:59:53 +0900

    feat: 포트폴리오 레이아웃 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,322 @@
-import Image from "next/image";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Image,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from "@nextui-org/react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <header className="header">
+        <Navbar shouldHideOnScroll>
+          <NavbarBrand>
+            <h1 className="text-xl">YEJI PORTFOLIO</h1>
+          </NavbarBrand>
+          <NavbarContent className="hidden sm:flex gap-4" justify="center">
+            <NavbarItem>
+              <Link href="#about" className="hover:text-[#755139]">
+                about
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link href="#projects" className="hover:text-[#755139]">
+                projects
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link href="#contact" className="hover:text-[#755139]">
+                contact
+              </Link>
+            </NavbarItem>
+          </NavbarContent>
+        </Navbar>
+      </header>
+      <main>
+        <section className="main">
+          <div className="container text-[#161b21]">
+            <article>
+              <h1 className="text-8xl font-bold drop-shadow-xl">
+                YEJI PORTFOLIO
+              </h1>
+              <h2 className="text-4xl font-semibold drop-shadow-xl">
+                Front-end Developer
+              </h2>
+            </article>
+          </div>
+        </section>
+        <section id="about" className="about">
+          <div className="container p-8 ">
+            <p className="text-center font-semibold text-3xl drop-shadow-md">
+              about
+            </p>
+            <Divider className="my-4" />
+            <article className="grid gap-4 grid-cols-2">
+              <Card>
+                <CardBody className="my-3">
+                  <p className="underline decoration-indigo-500 hover:decoration-red-400 text-center text-xl my-3">
+                    About
+                  </p>
+                  <ul className="marker:text-indigo-400 list-disc pl-5 space-y-3">
+                    <li>박예지</li>
+                    <li>경기도 시흥시</li>
+                    <li>1995. 10. 21.</li>
+                  </ul>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardBody className="my-3">
+                  <p className="underline decoration-indigo-500 hover:decoration-red-400 text-center text-xl my-3">
+                    Skills
+                  </p>
+                  <ul className="marker:text-indigo-400 list-disc pl-5 space-y-3">
+                    <li>
+                      <span>Front-end</span>
+                      <ul className="grid gap-1 *:rounded-full *:border *:border-indigo-100 *:bg-indigo-50 *:px-2 *:py-0.5 dark:text-indigo-300 dark:*:border-indigo-500/15 dark:*:bg-indigo-500/10 flex flex-wrap">
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>JavaScript</li>
+                        <li>jQuery</li>
+                        <li>React</li>
+                        <li>Next.js</li>
+                        <li>TypeScript</li>
+                      </ul>
+                    </li>
+                    <li>
+                      <span>Back-end</span>
+                      <ul className="grid gap-1 *:rounded-full *:border *:border-indigo-100 *:bg-indigo-50 *:px-2 *:py-0.5 dark:text-indigo-300 dark:*:border-indigo-500/15 dark:*:bg-indigo-500/10 flex flex-wrap">
+                        <li>Java</li>
+                        <li>JSP</li>
+                        <li>Spring Boot</li>
+                        <li>SQL</li>
+                        <li>JPA</li>
+                      </ul>
+                    </li>
+                    <li>
+                      <span>클라우드 플랫폼</span>
+                      <ul className="grid gap-1 *:rounded-full *:border *:border-indigo-100 *:bg-indigo-50 *:px-2 *:py-0.5 dark:text-indigo-300 dark:*:border-indigo-500/15 dark:*:bg-indigo-500/10 flex flex-wrap">
+                        <li>AWS</li>
+                      </ul>
+                    </li>
+                    <li>
+                      <span>버전 관리 시스템</span>
+                      <ul className="grid gap-1 *:rounded-full *:border *:border-indigo-100 *:bg-indigo-50 *:px-2 *:py-0.5 dark:text-indigo-300 dark:*:border-indigo-500/15 dark:*:bg-indigo-500/10 flex flex-wrap">
+                        <li>Git</li>
+                      </ul>
+                    </li>
+                    <li>
+                      <span>디자인 툴</span>
+                      <ul className="grid gap-1 *:rounded-full *:border *:border-indigo-100 *:bg-indigo-50 *:px-2 *:py-0.5 dark:text-indigo-300 dark:*:border-indigo-500/15 dark:*:bg-indigo-500/10 flex flex-wrap">
+                        <li>Figma</li>
+                        <li>Illustrator</li>
+                        <li>Photoshop</li>
+                      </ul>
+                    </li>
+                  </ul>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardBody className="my-3">
+                  <p className="underline decoration-indigo-500 hover:decoration-red-400 text-center text-xl my-3">
+                    Education
+                  </p>
+                  <ul className="marker:text-indigo-400 list-disc pl-5 space-y-3">
+                    <li>
+                      웹표준디지털디자인(웹디자인&웹퍼블리셔)
+                      <br />
+                      2019-03 ~ 2019-09
+                    </li>
+                    <li>
+                      자바(JAVA) 웹개발 프로그래밍
+                      <br />
+                      2022-12 ~ 2022-05
+                    </li>
+                  </ul>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardBody className="my-3">
+                  <p className="underline decoration-indigo-500 hover:decoration-red-400 text-center text-xl my-3">
+                    Experience
+                  </p>
+                  <ul className="marker:text-indigo-400 list-disc pl-5 space-y-3">
+                    <li>
+                      Web Publishing
+                      <br />
+                      2021-02 ~ 2021-08
+                    </li>
+                    <li>
+                      Software Development
+                      <br />
+                      2023-11 ~ 2024-02
+                    </li>
+                  </ul>
+                </CardBody>
+              </Card>
+            </article>
+          </div>
+        </section>
+        <section id="projects" className="projects">
+          <div className="container p-8">
+            <p className="text-center font-semibold text-3xl drop-shadow-md">
+              projects
+            </p>
+            <Divider className="my-4" />
+            <div className="grid gap-4">
+              <article>
+                <Card>
+                  <CardHeader className="flex flex-col">
+                    <h3 className="underline decoration-indigo-500 hover:decoration-red-400 text-center text-xl my-3">
+                      Coffee Store
+                    </h3>
+                    <p>React Project</p>
+                    <Divider className="my-4" />
+                  </CardHeader>
+                  <CardBody>
+                    <Image isZoomed alt="cafe images" src="./cafe.png" />
+                  </CardBody>
+                  <CardFooter className="flex flex-col">
+                    <div>
+                      <p>기능</p>
+                      <ul className="marker:text-indigo-400 list-disc pl-5 space-y-3">
+                        <li>로그인 / 회원가입</li>
+                        <li>상품 장바구니 담기 / 구매</li>
+                        <li>구매한 상품 리뷰 작성</li>
+                        <li>리뷰 / 공지사항</li>
+                      </ul>
+                    </div>
+                    <Divider className="my-4" />
+                    <Button
+                      href="https://main--yeji-portfolio-cafe-project.netlify.app/"
+                      as={Link}
+                      color="primary"
+                      variant="solid"
+                      target="_blank"
+                    >
+                      보러 가기
+                      <Image
+                        className="w-4"
+                        alt="mouse images"
+                        src="./mouse.svg"
+                      ></Image>
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </article>
+              <article>
+                <Card>
+                  <CardHeader className="flex flex-col">
+                    <h3 className="underline decoration-indigo-500 hover:decoration-red-400 text-center text-xl my-3">
+                      Portfolio
+                    </h3>
+                    <p>Next.js Project</p>
+                    <Divider className="my-4" />
+                  </CardHeader>
+                  <CardBody>
+                    <Image
+                      isZoomed
+                      alt="portfolio images"
+                      src="./portfolio.png"
+                    />
+                  </CardBody>
+                  <CardFooter className="flex flex-col">
+                    <div></div>
+                    <Divider className="my-4" />
+                  </CardFooter>
+                </Card>
+              </article>
+              <article>
+                <Card>
+                  <CardHeader className="flex flex-col">
+                    <h3 className="underline decoration-indigo-500 hover:decoration-red-400 text-center text-xl my-3">
+                      오목
+                    </h3>
+                    <p>JavaScript Project</p>
+                    <Divider className="my-4" />
+                  </CardHeader>
+                  <CardBody>
+                    <Image isZoomed alt="omok images" src="./omok.png" />
+                  </CardBody>
+                  <CardFooter className="flex flex-col">
+                    <div></div>
+                    <Divider className="my-4" />
+                    <Button
+                      href="./omok/index.html"
+                      as={Link}
+                      color="primary"
+                      variant="solid"
+                      target="_blank"
+                    >
+                      보러 가기
+                      <Image
+                        className="w-4"
+                        alt="mouse images"
+                        src="./mouse.svg"
+                      ></Image>
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </article>
+              <article>
+                <Card>
+                  <CardHeader className="flex flex-col">
+                    <h3 className="underline decoration-indigo-500 hover:decoration-red-400 text-center text-xl my-3">
+                      Book Search
+                    </h3>
+                    <p>API Project</p>
+                    <Divider className="my-4" />
+                  </CardHeader>
+                  <CardBody>
+                    <Image
+                      isZoomed
+                      alt="book search images"
+                      src="./booksearch.png"
+                    />
+                  </CardBody>
+                  <CardFooter className="flex flex-col">
+                    <div></div>
+                    <Divider className="my-4" />
+                    <Button
+                      href="./booksearch/index.html"
+                      as={Link}
+                      color="primary"
+                      variant="solid"
+                      target="_blank"
+                    >
+                      보러 가기
+                      <Image
+                        className="w-4"
+                        alt="mouse images"
+                        src="./mouse.svg"
+                      ></Image>
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </article>
+            </div>
+          </div>
+        </section>
+        <section id="contact" className="contact">
+          <div className="container p-8 text-[white]">
+            <p className="font-semibold text-3xl drop-shadow-md">contact</p>
+            <Divider className="my-4" />
+            <article>
+              <ul>
+                <li>📞 010-6472-2135</li>
+                <li>✉️ 1995.10.21@hanmail.net</li>
+              </ul>
+            </article>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
