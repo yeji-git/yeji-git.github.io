da7391575c2cc961d7b98a252c0bee109d19d401
yeji-git <ye-end@daum.net>
2024-03-04 16:20:27 +0900

    fix: next.config.mjs headers 추가

--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,20 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "upgrade-insecure-requests",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
