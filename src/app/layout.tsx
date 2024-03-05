2f8bc6fb3848bc16ad7ef8114fa98d12df64d577
yeji-git <ye-end@daum.net>
2024-03-05 14:19:20 +0900

    feat: font 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import TopButton from "./components/TopButton";
 import "./globals.css";
 import { Providers } from "./providers";
+import { Noto_Sans_KR } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const noto = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "YEJI PROTFOLIO",
@@ -21,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="light">
-      <body className={inter.className}>
+      <body className={noto.className}>
         <Providers>
           {children}
           <TopButton />
